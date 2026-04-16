9deefd2e1325e51eb843dc1814ecbc917edca218
nafismzsultan-ops <nafismzsultan@gmail.com>
2026-04-16 08:16:14 +0700

    Add files via upload

---
+++ 01-variabel.js
@@ -0,0 +1,18 @@
+let nama = 'Budi';
+let umur = 20;
+let kota = 'Banda Aceh';
+
+console.log('===Data Mahasiswa===');
+console.log('Nama :', nama);
+console.log('Umur :', umur);
+console.log('Kota :', kota);
+
+
+umur = 21;
+console.log('Ulang tahun! Umur sekarang:', umur);
+
+const Jurusan    = 'Teknik Informatika';
+const TahunMasuk = 2023;
+
+console.log('Jurusan      :', Jurusan);
+console.log('Tahun Masuk  :', TahunMasuk);
